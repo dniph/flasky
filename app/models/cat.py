8f73b7c4e15b9472861d75f68a2113fe3defef07
Dennif <dennif.pa@gmail.com>
2025-04-17 14:11:58 -0700

    cats bp added

---
+++ app/models/cat.py
@@ -0,0 +1,11 @@
+class Cat:
+    def __init__(self, id, name, color, personality):
+        self.id = id 
+        self.name = name
+        self.color = color
+        self.personality = personality
+
+cats = [
+    Cat(1, "Luna", "grey", "calm"),
+    Cat(2, "Ash", "orange", "cool")
+]
