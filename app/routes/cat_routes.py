5c436e90c78da5e52b6af3b8e1d5f71905f14a9e
Dennif <dennif.pa@gmail.com>
2025-04-18 10:30:03 -0700

    Get one cat by id added

--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -1,8 +1,11 @@
 from flask import Blueprint
 from ..models.cat import cats
 #doble punto te devuelve al directorio de app
+
 cats_bp = Blueprint("cats_bp", __name__, url_prefix="/cats")
 
+
+#Get informastion for a single cat 
 @cats_bp.get("")
 def get_all_cats():
     results_list = []
@@ -16,3 +19,26 @@ def get_all_cats():
         ))
         
     return results_list
+
+
+@cats_bp.get("/<cat_id>")
+def get_cat_by_id(cat_id):
+    try:
+        cat_id = int(cat_id)
+    except ValueError:
+        return {"message": f"Cat {cat_id} invalid"}, 400
+    
+    
+    for cat in cats:
+        if cat.id == cat_id:
+            return dict(
+                id = cat.id,
+                name = cat.name,
+                color = cat.color,
+                personality = cat.personality
+                
+            )
+#If cat Id does NOT exist            
+    return {"message": f"Cat {cat_id} not found"}, 404
+            
+    
